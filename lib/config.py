4283aae5b62d75199715936bb957b7f4e2a46d89
Fabio Ugo Venchiarutti <fabio@vuole.me>
2017-01-23 02:54:01 +0000

    - Added actual configuration loader/validator

--- lib/config.py
+++ lib/config.py
@@ -7,12 +7,21 @@
 import os
 import json
 import logging
-import logging.config
+import configparser
+import re
 
 import info
 
 
+# The following is a wild guess based on the relative path to the librariy's own
+PGPLEX_BASE_PATH = os.path.realpath(os.path.dirname(__file__) + "/..")
+PGPLEX_CONFIG_PATH = PGPLEX_BASE_PATH + "/etc"
+PGPLEX_SSL_PATH = PGPLEX_CONFIG_PATH + "/ssl"
+
+
+
 LOGGER = logging.getLogger(__name__)
+
 
 CONFIG_DIR_RELATIVE = "." + info.APP_NAME
 
@@ -32,8 +41,20 @@ CONFIG_DIR_RELATIVE = "." + info.APP_NAME
 # 		For multiple-choice settings it is the index in the options list
 # 4) Description of the setting. Defaults to None
 
+# Example:
+# GUC_DEFINITIONS = {
+#  "barista": {
+#   "name": (str, None, "Frank", "Just how [s]he's called"),
+#   "accept_tips": (bool, None, True, "U.S. are weird"),
+#   "shot_ratio": (float, (0.2, 0.4), 0.3, "Coffee/Total fraction"),
+#   "greeting_line": (str, ( "Hello, what would you like today?", "What's your poison?", "Hello, how can I help you?" ], 1, "What [s]he says"),
+#   "home_page": (str, re.compile("http://[.]*"), None, "Where [s]he's at")
+#  }
+# }
+
 GUC_DEFINITIONS = {
 	"global": {
+		"config_file": (str, None, PGPLEX_CONFIG_PATH + "/pgplex.conf", "Path of the configuration file. As a matter of fact, can only be affected from the command line or environment (the former overrides the latter)"),
 		"control_db_name": (str, None, "pgplex_control", """
 			A special database name that can is reserved for connections to the pgplex run-time interface.
 			Connection to this database cause queries to be routed to an internal management engine to
@@ -44,18 +65,22 @@ GUC_DEFINITIONS = {
 
 		"listen_addresses": (str, None, "localhost", "postgresql-equivalent, see https://www.postgresql.org/docs/current/static/runtime-config-connection.html#RUNTIME-CONFIG-CONNECTION-SETTINGS"),
 		"port": (int, (1, 65535), 5432, "postgresql-equivalent, see https://www.postgresql.org/docs/current/static/runtime-config-connection.html#RUNTIME-CONFIG-CONNECTION-SETTINGS"),
-		"max_connections": (int, (0,), 1024, "postgresql-equivalent, see https://www.postgresql.org/docs/current/static/runtime-config-connection.html#RUNTIME-CONFIG-CONNECTION-SETTINGS . Note that this applies to incoming connections and does not encroach shared memory"),
+		"max_connections": (int, (0, ), 3, "postgresql-equivalent, see https://www.postgresql.org/docs/current/static/runtime-config-connection.html#RUNTIME-CONFIG-CONNECTION-SETTINGS . Note that this applies to incoming connections and does not encroach shared memory"),
 		"max_connections_control_db": (int, (1,), 8, "much like max_connections, but it limits connections to the control_db instead. NOTE: connections to the system db count towards the global max_connections limit"),
 
 		"unix_socket_directories": (str, None, "/tmp", "postgresql-equivalent, see https://www.postgresql.org/docs/current/static/runtime-config-connection.html#RUNTIME-CONFIG-CONNECTION-SETTINGS"),
 		"unix_socket_group": (str, None, None, "postgresql-equivalent, see https://www.postgresql.org/docs/current/static/runtime-config-connection.html#RUNTIME-CONFIG-CONNECTION-SETTINGS"),
 		"unix_socket_permissions": (str, None, None, "postgresql-equivalent, see https://www.postgresql.org/docs/current/static/runtime-config-connection.html#RUNTIME-CONFIG-CONNECTION-SETTINGS"),
 
-		"tcp_keepalives_idle": (int, (0, ), 0, "postgresql-equivalent, see https://www.postgresql.org/docs/current/static/runtime-config-connection.html#RUNTIME-CONFIG-CONNECTION-SETTINGS"),
-		"tcp_keepalives_interval": (int, (0, ), 0, "postgresql-equivalent, see https://www.postgresql.org/docs/current/static/runtime-config-connection.html#RUNTIME-CONFIG-CONNECTION-SETTINGS"),
-		"tcp_keepalives_count": (int, (0, ), 0, "postgresql-equivalent, see https://www.postgresql.org/docs/current/static/runtime-config-connection.html#RUNTIME-CONFIG-CONNECTION-SETTINGS"),
-		
-		
+		"tcp_keepalives_idle": (int, (0,), 0, "postgresql-equivalent, see https://www.postgresql.org/docs/current/static/runtime-config-connection.html#RUNTIME-CONFIG-CONNECTION-SETTINGS"),
+		"tcp_keepalives_interval": (int, (0,), 0, "postgresql-equivalent, see https://www.postgresql.org/docs/current/static/runtime-config-connection.html#RUNTIME-CONFIG-CONNECTION-SETTINGS"),
+		"tcp_keepalives_count": (int, (0,), 0, "postgresql-equivalent, see https://www.postgresql.org/docs/current/static/runtime-config-connection.html#RUNTIME-CONFIG-CONNECTION-SETTINGS"),
+
+
+		"ssl": (bool, None, False, "postgresql-equivalent, see https://www.postgresql.org/docs/9.6/static/runtime-config-connection.html#RUNTIME-CONFIG-CONNECTION-SECURITY"),
+		"ssl_key_file": (str, None, PGPLEX_SSL_PATH + "/server.key", "postgresql-equivalent, see https://www.postgresql.org/docs/9.6/static/runtime-config-connection.html#RUNTIME-CONFIG-CONNECTION-SECURITY"),
+		"ssl_ca_file": (str, None, PGPLEX_SSL_PATH + "/ca.crt", "postgresql-equivalent, see https://www.postgresql.org/docs/9.6/static/runtime-config-connection.html#RUNTIME-CONFIG-CONNECTION-SECURITY")
+
 	},
 	"multiplexer": {
 		"channel_timeout": (int, (-1,), 0, """
@@ -88,10 +113,145 @@ GUC_DEFINITIONS = {
 }
 
 
-guc = {}
-# we first initialize a GUC with the default settings
-for section_name, section_settings in GUC_DEFINITIONS.items():
-	guc[section_name] = {}
-	for (setting_name, setting_def) in section_settings.items():
-		guc[section_name][setting_name] = setting_def[0](setting_def[2])
+
+
+def get_guc_defaults():
+	out_guc = {}
+	# we first initialize a GUC with the default settings
+	for section_name, section_settings in GUC_DEFINITIONS.items():
+		out_guc[section_name] = {}
+		for (setting_name, setting_def) in section_settings.items():
+			# strings have a different treatment if there are conditional options
+			if ((setting_def[0] is str) and isinstance(setting_def[1], (tuple, list))):
+				# we pick the member in the multiple options
+				out_guc[section_name][setting_name] = setting_def[0](setting_def[1][setting_def[2]])
+			else:
+				out_guc[section_name][setting_name] = setting_def[0](setting_def[2])
+	return out_guc
+
+
+
+def get_guc_file(cfg_file):
+	"""
+		Loads the configuration file and returns a nested dictionarly with the
+		same keys as the guc, but only showing the keys that ARE set in the
+		configuration file.
+		
+		Raises an exception on any error
+		
+		Args:
+		
+		cfg_file:				(str)The path of the config file
+		
+		
+		Return value:
+			the configuration array (ideally used to overlay it with the guc)
+	"""
+	out_guc = {}
+	guc_fp = open(cfg_file, "r")
+	cfg = configparser.ConfigParser()
+	cfg.readfp(guc_fp)
+	for sec_name in cfg.sections():
+		if (sec_name in (GUC_DEFINITIONS)):
+			out_guc[sec_name] = {}
+			for key_name, key_value in cfg[sec_name].items():
+				if (key_name in GUC_DEFINITIONS[sec_name]):
+					out_guc[sec_name][key_name] = cfg[sec_name][key_name]
+				else:
+					LOGGER.warning("Unknown configuration option `%s.%s` in `%s`" % (sec_name, key_name, cfg_file))
+		else:
+			LOGGER.warning("Unknown configuration section `%s` in `%s`" % (sec_name, cfg_file))
+	guc_fp.close()
+	return out_guc
+	
+	
+def get_guc_env():
+	"""
+		Generates a subset of the GUC tree from a set of known environment variables
+	"""
+	out_guc = {}
+	return out_guc
+
+
+def get_guc_cmdline():
+	"""
+		Generates a subset of the GUC tree from the command line arguments
+	"""
+	out_guc = {}
+	return out_guc
+
+
+def get_guc():
+	"""
+		Overlays configuration parameters in the correct order and returns
+		the configuration (generally, they're overwritten in the following order:
+		hardcoded_default, config_file, environment, command_line
+		
+		Returns the final configuration dictionary (which would be the new GUC).
+		
+		Also validates the configuration after the merger
+		
+		Raises exceptions at any failure
+	"""
+	out_guc = {}
+	# we now loop through the 4 functions to load the loop, in the order we want
+	# settings to be applied
+	for guc_f in (
+		get_guc_defaults,
+		get_guc_file,
+		get_guc_env,
+		get_guc_cmdline
+	):
+		# we conditionally prepare an argument list for the GUC loaders that require one.
+		guc_f_args = ({"cfg_file": out_guc["global"]["config_file"]} if (guc_f == get_guc_file) else {})
+		s_guc = guc_f(**guc_f_args)
+		# recursive merge of the array
+		for (guc_section, guc_keys) in s_guc.items():
+			if (guc_section not in (out_guc)):
+				out_guc[guc_section] = {}
+			for (guc_key, guc_value) in (guc_keys.items()):
+
+				target_type = GUC_DEFINITIONS[guc_section][guc_key][0]
+				
+				# we now try to validate stuff. type cast first
+				try:
+					final_val = target_type(guc_value)
+				except ValueError:
+					raise ValueError("Option `%s.%s` expects values of type `%s`. Set value `%s` (from %s) is invalid" % (
+						guc_section, guc_key, target_type.__name__, guc_value, guc_f.__name__
+					))
+					return None
+
+
+				# is there any check on the contents?
+				if (GUC_DEFINITIONS[guc_section][guc_key][1] is not None):
+					# yup. Checks depend on the type and specified parameters.
+					# we try to work them out
+					
+					if (target_type in (int, float)):
+						# we assume that GUC_DEFINITIONS is not broken
+						bond_l = target_type(GUC_DEFINITIONS[guc_section][guc_key][1][0])
+						bond_u = target_type(GUC_DEFINITIONS[guc_section][guc_key][1][1]) if (len(GUC_DEFINITIONS[guc_section][guc_key][1]) > 1) else None
+						num_format = ("%.9f" if (target_type is float) else "%d")
+						if ((final_val < bond_l) or ((bond_u is not None) and (final_val > bond_u))):
+							raise ValueError("Invalid value for `%s.%s`: `%s` (from %s) (must be between %s and %s)" % (
+								guc_section, guc_key, (num_format % (final_val,)), guc_f.__name__,
+								((num_format % (bond_l,)) if (bond_l is not None) else "-infinity"),
+								((num_format % (bond_u,)) if (bond_u is not None) else "+infinity")
+							))
+
+				# multiple choices or regular expressions...
+				if (target_type is str):
+					str_validator = GUC_DEFINITIONS[guc_section][guc_key][1]
+					if (isinstance(str_validator, (tuple, list))):
+						if (final_val not in (str_validator)):
+							raise ValueError("Invalid value for `%s.%s`: `%s` (from %s) (accepted values are `%s`)" % (
+								guc_section, guc_key, final_val, guc_f.__name__, "`, `".join(str_validator)
+							))
+					
+
+				out_guc[guc_section][guc_key] = guc_value
+	
+	return out_guc
+
 
